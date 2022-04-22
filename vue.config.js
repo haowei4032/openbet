8c8575e10520af4e38afb57503cba3de32902eda
haowei4032 <boss@haowei.me>
2022-04-23 00:36:35 +0800

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+    lintOnSave: false,
+    assetsDir: 'assets',
+    productionSourceMap: false,
+    outputDir: 'docs',
 })
